8bcfa25320da7efe1cb10a955d78ab4d15d7b6ae
Antonio Gamboa <toni_1999@live.com.mx>
2019-10-16 13:51:03 -0500

    Se agregaron carpetas BackEnd y FrontEnd para un mejor orden y se configuro la cadena de concexion de cada proyecto e nel AppConfing, se creo el DbContext para quitar el plural de las tablas.

---
+++ ClearAccount/ClearAccount.BussinessEntities/ClearAccountDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearAccount.BussinessEntities
+{
+    public class ClearAccountDbContext : DbContext
+    {
+        public ClearAccountDbContext() : base("ClearAccountDbContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
